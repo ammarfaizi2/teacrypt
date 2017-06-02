8a81af03db8d43465578f904ac8f958f1af2a544
Ammar Faizi <ammarfaizi2@gmail.com>
2017-06-02 12:42:23 +0700

    Update e example

--- example.php
+++ example.php
@@ -10,11 +10,21 @@ use Teacrypt\Teacrypt;
 
 $string = "hello world";
 $key    = "redangel";
-$encrypted_string = Teacrypt::encrypt($string, $key);
-$decrypted_string = Teacrypt::decrypt($encrypted_string, "redangel");
-var_dump(
-    array(
-            "encrypted_string" => $encrypted_string,
-            "decrypted_string" => $decrypted_string
-        )
-    );
+$encrypted_string1 = Teacrypt::encrypt($string, $key);
+$decrypted_string2 = Teacrypt::decrypt($encrypted_string1, $key);
+
+$encrypted_string2 = Teacrypt::encrypt($string, $key);
+$decrypted_string2 = Teacrypt::decrypt($encrypted_string2, $key);
+
+?><pre><?php
+var_dump(array(
+		"hasil kesamaan enkripsi 1 dan 2" => ($encrypted_string1 == $encrypted_string2),
+		"encrypted_string" => array($encrypted_string1, $encrypted_string2),
+		"decrypted_string" => array($decrypted_string2, $decrypted_string2)
+	));
+?></pre>
+
+<h3>Code file ini :</h3>
+
+<?php show_source(__FILE__); ?>
+
